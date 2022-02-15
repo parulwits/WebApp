8ae85b1ac6fe48253a372bcee93b29e340e60988
Parul <parul@WIL-Admins-Mac-mini.local>
2022-02-15 12:09:33 +0530

    15thFeb22

--- src/containers/Initial.js
+++ src/containers/Initial.js
@@ -1,18 +1,33 @@
+// App.js
 
-import React, { useState } from 'react';
-import {StyleSheet,Text, View,} from 'react-native';
-export default function Initial() {
-  
-    return (
-      <View style={styles.bg}>
-         <Text>Hello</Text>
-      </View>
-    )
-  }
+import React from "react";
+import WebView from "react-native-webview";
+import { useBridge } from "react-native-react-bridge";
+import webApp from "./WebScreen";
 
-  const styles = StyleSheet.create({
-    bg: {
-      flex:1,
-     backgroundColor:'red'
+const Initial = () => {
+  // useBridge hook create props for WebView and handle communication
+  // The argument is callback to receive message from React
+  const { ref, onMessage, emit } = useBridge((message) => {
+    console.log('message from WebView', message)
+    // emit sends message to React
+    //   type: event name
+    //   data: some data which will be serialized by JSON.stringify
+    if (message.type === "hello" && message.data === 123) {
+      console.log('here')
+      emit({ type: "success", data: "succeeded!" });
     }
   });
+
+  return (
+    <WebView
+      // ref, source and onMessage must be passed to react-native-webview
+      ref={ref}
+      // Pass the source code of React app
+      source={{ html: webApp }}
+      onMessage={onMessage}
+    />
+  );
+};
+
+export default Initial;
--- src/containers/WebScreen.js
+++ src/containers/WebScreen.js
@@ -1,11 +1,41 @@
+import React, { useState } from "react";
+import {
+  webViewRender,
+  emit,
+  useSubscribe,
+} from "react-native-react-bridge/lib/web";
+import './App.css'
+const Root = () => {
+  const [data, setData] = useState("");
+  // useSubscribe hook receives message from React Native
+  useSubscribe((message) => {
+    console.log('message from App', message)
+    if (message.type === "success") {
+      setData(message.data);
+    }
+  });
+  const onSubmit = () => {
+    alert('ffffff')
+    emit({ type: "hello", data: 123 });
+  };
+  return (
+    <div className='container'>
+      <div className='wrapper'>
+        Information Form {data}
+        <form onSubmit={onSubmit}>
+          <div className='form_section'>
+            <input type='text' placeholder='Name'></input>
+            <input type='text' placeholder='Age'></input>
+            <input type='text' placeholder='City'></input>
+            <input type='number' placeholder='Phone No.'></input>
+            <input type='submit'></input>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-import React, { useState } from 'react';
-import {StyleSheet} from 'react-native';
-export default function WebScreen() {
-  
-    return (
-        <View style={{backgroundColor:'yellow'}}>
-         
-      </View>
-    )
-  }
+// This statement is detected by babelTransformer as an entry point
+// All dependencies are resolved, compressed and stringified into one file
+export default webViewRender(<Root />);
